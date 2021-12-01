cea00d076ee80e6fc1ae7b6a36944ed141981777
IgnatevD <ignatevds@gmail.com>
2021-12-01 22:53:53 +0200

    Форма

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
 const listCakes = document.querySelector('.list-cakes');
 const modalOpen = document.querySelector('.js-lightbox');
-const modalBd = document.querySelector('.lightbox__overlay');
 const modalClose = document.querySelector('.lightbox__button');
 
 listCakes.addEventListener(`click`, openMobalBd);
-modalBd.addEventListener(`click`, closeMobalBd);
 modalClose.addEventListener(`click`, deleteClassIsOpen);
 
 function openMobalBd(e) {
@@ -14,10 +12,4 @@ function openMobalBd(e) {
 
 function deleteClassIsOpen() {
   modalOpen.classList.remove(`is-open`);
-}
-
-function closeMobalBd(e) {
-  if (e.currentTarget === e.target) {
-    deleteClassIsOpen();
-  }
 }
