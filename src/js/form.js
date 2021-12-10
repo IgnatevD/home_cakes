7ba4c3b1d7d980c9b8d2467cdb175d2f24f2ca4e
IgnatevD <ignatevds@gmail.com>
2021-12-10 11:44:20 +0200

    Дополнены поля формы

--- src/js/form.js
+++ src/js/form.js
@@ -5,11 +5,12 @@ const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}
 
 const form = document.querySelector('.form');
 const post = document.querySelector('.post');
+document.getElementById('date').value = new Date().toISOString().substring(0, 10);
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   const {
-    elements: { name, telefon, date, time, holiday, cake, pick, weight, filling },
+    elements: { name, telefon, date, time, holiday, cake, pick, weight, filling, adress, delivery },
   } = e.currentTarget;
   console.log(pick.files);
   const mes = `
@@ -20,9 +21,10 @@ form.addEventListener('submit', e => {
   Время: ${time.value},
   Какой праздник: ${holiday.value},
   Какой декор: ${cake.value},
-  Картинкна: ${pick.files[0]},
+  Картинкна: ${pick.files},
   Вес торта: ${weight.value},
 Начинка торта: ${filling.value}
+Адресс: ${delivery.value}, ${adress.value}
   `;
 
   fetch(`${url}${mes}`);
